d536923942730af6ae308a05b484c291f82b4fe5
KravetsBohdan <bodyakravets@gmail.com>
2022-10-10 21:05:54 +0300

    add feature to generate random number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        int luckyNumber = new Random().nextInt(10) + 1;
+        System.out.println("Your lucky number " + luckyNumber);
 
     }
 }
